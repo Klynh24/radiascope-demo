cb38847ae3dd93376c3709ff918274f64c45ce6b
Klynh24 <linhngocj1113@gmail.com>
2024-10-13 00:26:01 +0700

    add

--- js/server.js
+++ js/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000; // Chọn cổng bạn muốn
+const port = 3000; /*giả sử */
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,13 +11,13 @@ app.post('/process_form', (req, res) => {
   const phone = req.body.phone;
   const message = req.body.message;
 
-  // Xử lý dữ liệu ở đây (ví dụ: lưu vào cơ sở dữ liệu, gửi email, v.v.)
+ 
   console.log('Dữ liệu nhận được:', { name, email, phone, message });
 
-  // Gửi phản hồi
+ 
   res.send('Cảm ơn bạn đã gửi form!');
 });
 
 app.listen(port, () => {
-  console.log(`Máy chủ đang chạy tại http://localhost:${port}`);
+  console.log(`Máy chủ đang chạy tại http://localhost:${port}`); /*giả sử */
 });
